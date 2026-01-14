711f050358a3354b1d0c6fe28ad5fefde065a873
ok790 <50056485@belfastmet.ac.uk>
2026-01-14 10:28:16 +0000

    # Add base Tkinter canvas with grid and mouse drawing creating base tkinter canvas
    - Created a Tkinter canvas with specified width, height, and background
    - Added draw_grid() function to display a visual grid
    - Added draw_on_mouse() function to draw lines following mouse movement
    - Bound draw_on_mouse() to '<Motion>' event to track mouse movement

---
+++ HCT_Simulator.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+def draw_grid():
+    # draw vertical lines
+    for x in range(0, canvas_width + 1, cell_size):
+        canvas.create_line(x, 0, x, canvas_height, fill='white')
+
+    # draw horizontal lines
+    for y in range(0, canvas_height + 1, cell_size):
+        canvas.create_line(0, y, canvas_width, y, fill='white')
+
+
+# e is automatically passed by Tkinter when the even occurs
+def draw_on_mouse(e):
+    # access global variable as we cannot return values from this function
+    global last_pos
+
+    x = e.x
+    y = e.y
+
+    if last_pos != None:
+        canvas.create_line(last_pos[0], last_pos[1], x, y, fill='white')
+
+    last_pos = (x, y)
+
+
+root = Tk()
+
+# initialising variable values
+last_pos = None
+canvas_width = 1920
+canvas_height = 1080
+cell_size = 80
+
+
+# creating base canvas
+canvas = Canvas(root, bg='grey', width=canvas_width, height=canvas_height)
+canvas.grid(row=0, column=0, sticky=(N, W, E, S))
+
+# drawing visual grid
+draw_grid()
+
+# call event
+canvas.bind('<Motion>',draw_on_mouse)
+
+root.mainloop()
+
