389671713c9baaa805b5e15f1c553bdf4f8f477a
ok790 <50056485@belfastmet.ac.uk>
2026-01-15 16:10:50 +0000

    Created a 2d Data Structure cells - this will be used to track which pixels in which cells have been interacted with. Simulating already hoovered areas.

--- HCT_Simulator.py
+++ HCT_Simulator.py
@@ -24,6 +24,30 @@ def draw_on_mouse(e):
     last_pos = (x, y)
 
 
+def initialise_pixel_status():
+    # going over a row of cells
+    for row_of_cells in range(canvas_height//cell_size):
+        # initialising list which will hold data for all cells in current row
+        cell_row = []
+        # going over each cell in current row
+        for cell_in_row in range(canvas_width//cell_size):
+            # initialising list which will hold data for each individual cell in current row
+            cell = []
+            # going over each pixel in current cell
+            for row_of_pixels in range(cell_size):
+                # initialising list which will hold data for each pixel in current row of the current cell
+                pixel_row = []
+                # going over each pixel in current row of current cell
+                for pixel_in_row in range(cell_size):
+                    # add current pixels 'data' to pixel row
+                    pixel_row.append(False)
+                # all of current cells current row of pixel data has been set it can now be appended to the cell list
+                cell.append(pixel_row)
+            # all of current cells data has been set and can be appended to the cell_row before moving onto next cell in row
+            cell_row.append(cell)
+        cells.append(cell_row)
+
+
 root = Tk()
 
 # initialising variable values
@@ -32,6 +56,7 @@ canvas_width = 1920
 canvas_height = 1080
 cell_size = 80
 
+cells = []
 
 # creating base canvas
 canvas = Canvas(root, bg='grey', width=canvas_width, height=canvas_height)
@@ -42,6 +67,9 @@ draw_grid()
 
 # call event
 canvas.bind('<Motion>',draw_on_mouse)
+
+# initialise 2d data structure to false
+initialise_pixel_status()
 
 root.mainloop()
 
